c22454c9798e38701fed98edc150bba840f4a73f
Yuxuan Zhao <me@yuxuanz.com>
2020-01-31 20:16:43 -0700

    add initial design implementation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import "../styles/App.scss";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import PostBlock from "./PostBlock";
 
 function App() {
   return (
-    <Container className="app">
-      <Row className="header">
-          CMPUT 404 - Final Project
-      </Row>
+    <Container fluid className="app">
+      <PostBlock />
     </Container>
   );
 }
