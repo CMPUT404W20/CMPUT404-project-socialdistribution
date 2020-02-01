afef3745435c0f69ae222dafd1c788a1515c0c3f
Yuxuan Zhao <me@yuxuanz.com>
2020-02-01 16:33:57 -0700

    implement the dropdown

--- src/components/PostPopup.jsx
+++ src/components/PostPopup.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
+import "../styles/Postpopup.scss";
 import Dropdown from "react-bootstrap/Dropdown";
+import editIcon from "../staticfiles/edit-pencil.svg";
+import deleteIcon from "../staticfiles/del-bin.svg"
+import linkIcon from "../staticfiles/link.svg"
 
 class PostPopup extends Component {
   constructor(props) {
@@ -9,12 +13,21 @@ class PostPopup extends Component {
 
   render() {
     return (
-      <div className="post-popup">
-        <Dropdown.Item >Edit</Dropdown.Item>
+      <div>
+        <Dropdown.Item className="post-popup-item">
+          <img src={editIcon} alt="pencil-icon" />
+          <span className="post-popup-text">Edit</span>
+        </Dropdown.Item>
         <Dropdown.Divider />
-        <Dropdown.Item >Delete</Dropdown.Item>
+        <Dropdown.Item className="post-popup-item">
+          <img src={deleteIcon} alt="del-icon" />
+          <span className="post-popup-text">Delete</span>
+        </Dropdown.Item>
         <Dropdown.Divider />
-        <Dropdown.Item >Copy link to post</Dropdown.Item>
+        <Dropdown.Item className="post-popup-item">
+          <img src={linkIcon} alt="link-icon" />
+          <span className="post-popup-text">Copy link to post</span>
+        </Dropdown.Item>
       </div>
     );
   }
