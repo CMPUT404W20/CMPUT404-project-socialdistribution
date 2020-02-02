852b6ce28dbc27527f2dcdf6bd2da98c961b222d
Yuxuan Zhao <me@yuxuanz.com>
2020-02-02 15:51:54 -0700

    update the tests

--- src/tests/components/PostBlock.test.jsx
+++ src/tests/components/PostBlock.test.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
 import PostBlock from "../../components/PostBlock";
-import PostDropDown from "../../components/PostDropDown";
 import demoImage from "../../staticfiles/demo-img.png";
 
 describe("Post Block Components", () => {
@@ -18,20 +17,5 @@ describe("Post Block Components", () => {
       />,
     );
     expect(component).toMatchSnapshot();
-  });
-
-  it("dropdown should be rendered after clicking the more button", () => {
-    const component = shallow(
-      <PostBlock
-        imageSrc={demoImage}
-        username={user}
-        postTime={postTime}
-        content={content}
-      />,
-    );
-    const moreButton = component.find("#post-more-button");
-    moreButton.simulate("click");
-
-    expect(moreButton.find(PostDropDown).length).toBe(1);
   });
 });
