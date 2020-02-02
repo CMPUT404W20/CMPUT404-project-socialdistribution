9efe5524fde27e049baf448c4c4214e9451c3d90
Yuxuan Zhao <me@yuxuanz.com>
2020-02-02 16:25:55 -0700

    update app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,9 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import PostBlock from "./post/PostBlock";
-import demoImage from "../images/demo-img.png";
+import Login from "./Login";
 
 function App() {
-  const content = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
   return (
-    <Container fluid className="app">
-      <PostBlock
-        imageSrc={demoImage}
-        username="testuser"
-        postTime="10 hours ago"
-        content={content}
-      />
-    </Container>
+    <Login />
   );
 }
 
