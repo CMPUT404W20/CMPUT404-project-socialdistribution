7c1d003544a9423e69ebd2a8881c2f8eb549c6ac
ceciliaccwei <ceciliawei@yahoo.com>
2020-02-03 15:07:59 -0700

    post box in progress

--- src/components/MakePost.jsx
+++ src/components/MakePost.jsx
@@ -9,17 +9,22 @@ class MakePost extends Component {
   render() {
     return (
       <div className="makePost">
-        <div>
+        <div className="block">
           <div className="form-row-1">
             <b>NEW POST</b>
             <select className="privacy-select">
-              <option value="public">&#xf26e; fa-500px</option>
-              <option value="lime">Lime</option>
-              <option selected value="coconut">
-                Coconut
+              <option selected value="public">
+                Anyone
               </option>
-              <option value="mango">Mango</option>
+              <option value="another author">Specific author</option>
+              <option value="friends">Friends</option>
+              <option value="mutual friends">Mutual friends</option>
+              <option value="local friends">Local friends</option>
+              <option value="private">Private</option>
             </select>
+          </div>
+          <div className="row-2-input">
+            <textarea placeholder="What's on your mind?"></textarea>
           </div>
         </div>
       </div>
