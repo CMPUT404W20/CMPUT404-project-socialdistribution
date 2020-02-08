767ebe8a5a84ea4ffed4fad037ded9f97ae4b016
Maharsh Patel <patel@maharsh.net>
2020-02-07 22:21:55 -0700

    major cleanup

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,16 @@
 import React, { Component } from "react";
 import "../styles/NavBar.scss";
-import SearchRoundedIcon from "@material-ui/icons/SearchRounded";
 import HomeOutlinedIcon from "@material-ui/icons/HomeOutlined";
 import PeopleAltOutlinedIcon from "@material-ui/icons/PeopleAltOutlined";
 import NotificationsNoneOutlinedIcon from "@material-ui/icons/NotificationsNoneOutlined";
-import ExitToAppOutlinedIcon from "@material-ui/icons/ExitToAppOutlined";
 import PropTypes from "prop-types";
-import logo from "../images/logo.svg";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
-import Button from "react-bootstrap/Button";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import logo from "../images/logo.svg";
 
 // Example usage: <Navbar selected="Friends" />
 class NavBar extends Component {
@@ -20,7 +18,7 @@ class NavBar extends Component {
     super(props);
     this.state = {
       username: "Username",
-      notification: 1,
+      numNotifications: 2,
     };
   }
   // will implement search later. Disabled eslint for this method for now
@@ -34,7 +32,7 @@ class NavBar extends Component {
   }
 
   render() {
-    const { username, notification } = this.state;
+    const { username, numNotifications } = this.state;
     const { selected } = this.props;
     return (
       <Container className="header" fluid>
@@ -43,17 +41,11 @@ class NavBar extends Component {
             <img src={logo} width="85%" alt="app logo" />
           </Navbar.Brand>
           <div className="search-input-container">
-            {/* <input
-              className="search-input"
-              placeholder="Search"
-              onKeyDown={this.handleSubmit}
-            /> */}
             <InputGroup size="sm" className="searchBar">
               <FormControl
                 placeholder="Search"
                 aria-label="Search"
                 aria-describedby="basic-addon1"
-                className="formform"
               />
             </InputGroup>
           </div>
@@ -61,99 +53,32 @@ class NavBar extends Component {
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto" />
             <Nav>
-              <Nav.Link target="_blank" href="#">
+              <Nav.Link target="_blank" href="#" active={selected === "Home"}>
                 <HomeOutlinedIcon />
-                {/* <br />
-                Home */}
               </Nav.Link>
-              <Nav.Link target="_blank" href="#">
+              <Nav.Link target="_blank" href="#" active={selected === "Friends"}>
                 <PeopleAltOutlinedIcon />
-                {/* <br />
-                Friends */}
               </Nav.Link>
-              <Nav.Link target="_blank" href="#">
+              <Nav.Link target="_blank" href="#" active={selected === "Notifications"}>
                 <div className="icon-wrapper">
                   <NotificationsNoneOutlinedIcon />
                   <div className="badge-wrapper">
-                    <span className="badge">{2}</span>
+                    <span className="badge">{numNotifications}</span>
                   </div>
                 </div>
-                {/* <NotificationsNoneOutlinedIcon /> */}
-                {/* <br />
-                Notices */}
               </Nav.Link>
             </Nav>
             <Nav>
-              <Nav.Link target="_blank" href="#">
-                {/* <NotificationsNoneOutlinedIcon /> */}
-                username
-              </Nav.Link>
+              <NavDropdown title={username} id="basic-nav-dropdown" alignRight>
+                <NavDropdown.Item href="#">Profile</NavDropdown.Item>
+                <NavDropdown.Item href="#">Settings</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#">Logout</NavDropdown.Item>
+              </NavDropdown>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
       </Container>
-      // <div className="nav">
-      //   <div className="navRow">
-      //     <div className="left-menu">
-      //       <div className="logo-container">
-      //         <img src={logo} width="85%" alt="app logo" />
-      //       </div>
-      //       <div className="search-input-container">
-      //         <SearchRoundedIcon className="searchButton" />
-      //         <input
-      //           className="search-input"
-      //           placeholder="Search"
-      //           onKeyDown={this.handleSubmit}
-      //         />
-      //       </div>
-      //     </div>
-      //     <div className="right-side-menu">
-      //       <div className="icons">
-      //         <a
-      //           className={selected === "Home" ? "Home selected" : "Home"}
-      //           href=" "
-      //         >
-      //           <HomeOutlinedIcon />
-      //           <p>HOME</p>
-      //         </a>
-      //         <a
-      //           className={
-      //             selected === "Friends" ? "Friends selected" : "Friends"
-      //           }
-      //           href=" "
-      //         >
-      //           <PeopleAltOutlinedIcon />
-      //           <p>FRIENDS</p>
-      //         </a>
-      //         <a
-      //           className={
-      //             selected === "Notices" ? "Notices selected" : "Notices"
-      //           }
-      //           href=" "
-      //         >
-      //           <div className="icon-wrapper">
-      //             <NotificationsNoneOutlinedIcon />
-      //             <div className="badge-wrapper">
-      //               <span className="badge">{notification}</span>
-      //             </div>
-      //           </div>
-      //           <p>NOTICES</p>
-      //         </a>
-      //       </div>
-      //       <div className="user">
-      //         <a href=" " className={selected === "Username" ? "selected" : ""}>
-      //           <p>{username}</p>
-      //         </a>
-      //       </div>
-      //       <div className="log-out">
-      //         <a className="logout-div" href=" ">
-      //           <ExitToAppOutlinedIcon className="post-icon" />
-      //           <p>LOG OUT</p>
-      //         </a>
-      //       </div>
-      //     </div>
-      //   </div>
-      // </div>
     );
   }
 }
@@ -162,7 +87,7 @@ NavBar.propTypes = {
 };
 
 NavBar.defaultProps = {
-  selected: "",
+  selected: "Home",
 };
 
 export default NavBar;
