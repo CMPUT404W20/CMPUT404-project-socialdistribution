252c7fef03c4957e8838dd5bb2d1b82ef80ea73a
Maharsh Patel <patel@maharsh.net>
2020-02-07 23:14:29 -0700

    updates test file and removed useless tests

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -6,7 +6,6 @@ import NotificationsNoneOutlinedIcon from "@material-ui/icons/NotificationsNoneO
 import PropTypes from "prop-types";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Container";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import NavDropdown from "react-bootstrap/NavDropdown";
