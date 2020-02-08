5bd556473ebf17add94c708446a8b7c09375488b
Yuxuan Zhao <me@yuxuanz.com>
2020-02-07 20:04:06 -0700

    update admin

--- backend/admin.py
+++ backend/admin.py
@@ -1,20 +1,10 @@
 from django.contrib import admin
-from .models import User,Post,PostAccess,Comments,FriendRequest,Friend
+from .models import User, Post, PostAccess, Comments, FriendRequest, Friend
 
-#Registering User model
+# #Registering all Database Model
 admin.site.register(User)
-
-#Registering Post model
 admin.site.register(Post)
-
-#Registering PostAccess model
 admin.site.register(PostAccess)
-
-#Registering Comments model
 admin.site.register(Comments)
-
-#Registering FriendRequest model
 admin.site.register(FriendRequest)
-
-#Registering Friend model
 admin.site.register(Friend)
