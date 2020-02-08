6387b767164712e620f647014d515f4d144f212b
Maharsh Patel <patel@maharsh.net>
2020-02-08 00:09:08 -0700

    update tests

--- src/tests/components/post/MakePost.test.jsx
+++ src/tests/components/post/MakePost.test.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
-import MakePost from "../../components/MakePost";
+import MakePost from "../../../components/post/MakePost";
 
 describe("Post box Component", () => {
   it("should render correctly", () => {
