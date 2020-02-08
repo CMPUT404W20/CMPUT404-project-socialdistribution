4448439f937fb9e23151485602ea4f0c7dcb69eb
Maharsh Patel <patel@maharsh.net>
2020-02-07 21:08:27 -0700

    nav bar with buttons and search - mid work

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,6 +7,12 @@ import NotificationsNoneOutlinedIcon from "@material-ui/icons/NotificationsNoneO
 import ExitToAppOutlinedIcon from "@material-ui/icons/ExitToAppOutlined";
 import PropTypes from "prop-types";
 import logo from "../images/logo.svg";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
 
 // Example usage: <Navbar selected="Friends" />
 class NavBar extends Component {
@@ -31,68 +37,116 @@ class NavBar extends Component {
     const { username, notification } = this.state;
     const { selected } = this.props;
     return (
-      <div className="nav">
-        <div className="navRow">
-          <div className="left-menu">
-            <div className="logo-container">
-              <img src={logo} width="85%" alt="app logo" />
-            </div>
-            <div className="search-input-container">
-              <SearchRoundedIcon className="searchButton" />
-              <input
-                className="search-input"
+      <Container className="header" fluid>
+        <Navbar collapseOnSelect expand="sm">
+          <Navbar.Brand className="logo">
+            <img src={logo} width="85%" alt="app logo" />
+          </Navbar.Brand>
+          <div className="search-input-container">
+            {/* <input
+              className="search-input"
+              placeholder="Search"
+              onKeyDown={this.handleSubmit}
+            /> */}
+            <InputGroup size="sm" className="searchBar">
+              <FormControl
                 placeholder="Search"
-                onKeyDown={this.handleSubmit}
+                aria-label="Search"
+                aria-describedby="basic-addon1"
+                className="formform"
               />
-            </div>
+              <InputGroup.Append>
+                <Button variant="outline-secondary">
+                  <SearchRoundedIcon className="searchButton" />
+                </Button>
+              </InputGroup.Append>
+            </InputGroup>
           </div>
-          <div className="right-side-menu">
-            <div className="icons">
-              <a
-                className={selected === "Home" ? "Home selected" : "Home"}
-                href=" "
-              >
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto" />
+            <Nav>
+              <Nav.Link target="_blank" href="#">
                 <HomeOutlinedIcon />
-                <p>HOME</p>
-              </a>
-              <a
-                className={
-                  selected === "Friends" ? "Friends selected" : "Friends"
-                }
-                href=" "
-              >
+                <br />
+                Home
+              </Nav.Link>
+              <Nav.Link target="_blank" href="#">
                 <PeopleAltOutlinedIcon />
-                <p>FRIENDS</p>
-              </a>
-              <a
-                className={
-                  selected === "Notices" ? "Notices selected" : "Notices"
-                }
-                href=" "
-              >
-                <div className="icon-wrapper">
-                  <NotificationsNoneOutlinedIcon />
-                  <div className="badge-wrapper">
-                    <span className="badge">{notification}</span>
-                  </div>
-                </div>
-                <p>NOTICES</p>
-              </a>
-            </div>
-            <div className="user">
-              <a href=" " className={selected === "Username" ? "selected" : ""}>
-                <p>{username}</p>
-              </a>
-            </div>
-            <div className="log-out">
-              <a className="logout-div" href=" ">
-                <ExitToAppOutlinedIcon className="post-icon" />
-                <p>LOG OUT</p>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+                <br />
+                Friends
+              </Nav.Link>
+              <Nav.Link target="_blank" href="#">
+                <NotificationsNoneOutlinedIcon />
+                <br />
+                Notices
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Container>
+      // <div className="nav">
+      //   <div className="navRow">
+      //     <div className="left-menu">
+      //       <div className="logo-container">
+      //         <img src={logo} width="85%" alt="app logo" />
+      //       </div>
+      //       <div className="search-input-container">
+      //         <SearchRoundedIcon className="searchButton" />
+      //         <input
+      //           className="search-input"
+      //           placeholder="Search"
+      //           onKeyDown={this.handleSubmit}
+      //         />
+      //       </div>
+      //     </div>
+      //     <div className="right-side-menu">
+      //       <div className="icons">
+      //         <a
+      //           className={selected === "Home" ? "Home selected" : "Home"}
+      //           href=" "
+      //         >
+      //           <HomeOutlinedIcon />
+      //           <p>HOME</p>
+      //         </a>
+      //         <a
+      //           className={
+      //             selected === "Friends" ? "Friends selected" : "Friends"
+      //           }
+      //           href=" "
+      //         >
+      //           <PeopleAltOutlinedIcon />
+      //           <p>FRIENDS</p>
+      //         </a>
+      //         <a
+      //           className={
+      //             selected === "Notices" ? "Notices selected" : "Notices"
+      //           }
+      //           href=" "
+      //         >
+      //           <div className="icon-wrapper">
+      //             <NotificationsNoneOutlinedIcon />
+      //             <div className="badge-wrapper">
+      //               <span className="badge">{notification}</span>
+      //             </div>
+      //           </div>
+      //           <p>NOTICES</p>
+      //         </a>
+      //       </div>
+      //       <div className="user">
+      //         <a href=" " className={selected === "Username" ? "selected" : ""}>
+      //           <p>{username}</p>
+      //         </a>
+      //       </div>
+      //       <div className="log-out">
+      //         <a className="logout-div" href=" ">
+      //           <ExitToAppOutlinedIcon className="post-icon" />
+      //           <p>LOG OUT</p>
+      //         </a>
+      //       </div>
+      //     </div>
+      //   </div>
+      // </div>
     );
   }
 }
