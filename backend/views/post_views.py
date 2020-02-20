6a67d941f8cf9b15f16f14a428da07f4b1430d78
Yuxuan Zhao <me@yuxuanz.com>
2020-02-19 23:18:02 -0700

    add initial creating post api

--- backend/views/post_views.py
+++ backend/views/post_views.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework import mixins
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import permissions
 from rest_framework.decorators import permission_classes
 
@@ -23,13 +24,28 @@ class PostViewSet(mixins.CreateModelMixin,
     queryset = Post.objects.all()
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-
     def get(self, request, *args, **kwargs):
         # TODO add permission check for post
         return self.list(request, *args, **kwargs)
 
     def create_post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+        body = request.data
+        request_query = body.get("query")
+        post_data = body.get("post")
+
+        if request_query == "createPost" and post_data:
+            serializer = PostSerializer(data=post_data, context={"request": request})
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    {"query": "createPost", "success": True, "message": "Post created"}, status=status.HTTP_201_CREATED
+                )
+            else:
+                return Response({"query": "createPost", "success": False, "message": "wrong payload format"},
+                            status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+        else:
+            return Response({"query": "createPost", "success": False, "message": "wrong request"},
+                            status=status.HTTP_400_BAD_REQUEST)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
