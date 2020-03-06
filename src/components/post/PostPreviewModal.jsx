8ce21a7ccc1d9d5019480a2272a99e0edf4d6fbf
Maharsh Patel <patel@maharsh.net>
2020-03-05 18:36:40 -0700

    add conditional menu rendering for post

--- src/components/post/PostPreviewModal.jsx
+++ src/components/post/PostPreviewModal.jsx
@@ -2,27 +2,26 @@ import React from "react";
 import PropTypes from "prop-types";
 import "../../styles/post/PostPreviewModal.scss";
 import Modal from "react-bootstrap/Modal";
+import Post from "./Post";
+import demoImage from "../../images/demo-img.png";
 
 function PostPreviewModal(props) {
   const { onHide, show } = props;
 
   return (
-    <Modal onHide={onHide} show={show} className="upload-image-modal">
+    <Modal size="lg" onHide={onHide} show={show} className="post-preview-modal">
       <Modal.Header closeButton>
         <Modal.Title>Post Preview</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        Testing
+        <Post
+          username="username"
+          postTime={new Date()}
+          imageSrc={demoImage}
+          content="Some content"
+          previewMode
+        />
       </Modal.Body>
-      {/* <Modal.Footer className="upload-button-wrapper">
-        <button
-          type="button"
-          className="upload-button"
-          onClick={onHide}
-        >
-          Upload
-        </button>
-      </Modal.Footer> */}
     </Modal>
   );
 }
