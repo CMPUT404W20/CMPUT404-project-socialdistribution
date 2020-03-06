967f30ee0f8983af53c666df14c04cce6695b4b3
Maharsh Patel <patel@maharsh.net>
2020-03-05 17:20:15 -0700

    revert the sample text to simpler markdown

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -16,41 +16,6 @@ class Homepage extends Component {
 
   // eslint-disable-next-line class-methods-use-this
   renderPosts() {
-    // eslint-disable-next-line quotes
-    const mdContent = `# Heading 1
-## Heading 2
-### Heading 3
-#### Heading 4
-##### Heading 5
-###### Heading 6
-
-1. Bullet 1
-2. Bullet 2
-
-[Google Link](https://www.google.com)
-
-![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Some logo")
-
-Inline \`code\` 
-
-\`\`\`
-var s = "some code";
-alert(s);
-\`\`\`
-
-| Tables        | Are           | Cool  |
-| ------------- |:-------------:| -----:|
-| col 3 is      | right-aligned | $1600 |
-| col 2 is      | centered      |   $12 |
-| zebra stripes | are neat      |    $1 |
-
-> Blockquote
-
----
-
-Normal Text
-    `;
-
     return (
       <Row className="postWrapper">
         <Col md={2} />
@@ -59,7 +24,7 @@ Normal Text
             username="username"
             postTime={new Date()}
             imageSrc={demoImage}
-            content={mdContent}
+            content="Some content"
           />
         </Col>
         <Col md={2} />
