f5650d6fd73b45b73d40edfa3ccde8fcb8caf6be
Maharsh Patel <patel@maharsh.net>
2020-03-08 17:01:58 -0600

    replace default upload with dropzone

--- src/components/post/UploadImageModal.jsx
+++ src/components/post/UploadImageModal.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
 import PropTypes from "prop-types";
 import "../../styles/post/UploadImageModal.scss";
 import Modal from "react-bootstrap/Modal";
+
+
+function ImageDropzone() {
+  const onDrop = useCallback((acceptedFiles) => {
+    console.log(acceptedFiles);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ accept: "image/*", onDrop });
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ? <p>Drop the files here ...</p> : <p>Drag and drop some files here, or click to select files</p>
+      }
+    </div>
+  );
+}
 
 function UploadImageModal(props) {
   const { onHide, show } = props;
@@ -9,13 +28,10 @@ function UploadImageModal(props) {
   return (
     <Modal onHide={onHide} show={show} className="upload-image-modal">
       <Modal.Header closeButton>
-        <Modal.Title>Upload images</Modal.Title>
+        <Modal.Title>Upload Image</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <input
-          accept="image/*"
-          type="file"
-        />
+        <ImageDropzone />
       </Modal.Body>
       <Modal.Footer className="upload-button-wrapper">
         <button
