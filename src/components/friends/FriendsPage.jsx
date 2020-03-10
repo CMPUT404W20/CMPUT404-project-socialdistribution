6808977fa91499a852246b0c27676796a25fb75e
Cecilia Wei <35175404+ceciliaccwei@users.noreply.github.com>
2020-03-09 21:48:59 -0600

    Friends page and notification page (#67)
    * profile page initial commit
    
    * profile header section
    
    * image and username section done
    
    * finished header
    
    * eslint
    
    * profile page
    
    * eslint and app.jsx
    
    * changes based on comments
    
    * added edit profile modal
    
    * updated renderModal function in both profile header and makepost
    
    * friend item in progress
    
    * babel eslint?
    
    * friends page done
    
    * fix eslint
    
    * fix component name
    
    * noticePage
    
    * added noticePage
    
    * fix eslint dependency please work
    
    * updated color and bootstrap
    
    * cleaned cache
    
    * remove user type in profile page for self; changed border color for friends
    
    * fixed eslint, comments on pr, common style import

---
+++ src/components/friends/FriendsPage.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import "../../styles/friends-notices/Page.scss";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import PeopleAltOutlinedIcon from "@material-ui/icons/PeopleAltOutlined";
+import NavigationBar from "../NavigationBar";
+import FriendItem from "./FriendItem";
+
+class FriendsPage extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.state = {
+      friendsList: [{ id: "001", name: "Username1" }, { id: "002", name: "Username2" }, { id: "003", name: "Username3" }, { id: "004", name: "Username4" }, { id: "005", name: "Username5" }],
+    };
+  }
+
+  handleUnfollow(id) {
+    const { friendsList } = this.state;
+    const filteredList = friendsList.filter((item) => item.id !== id);
+    this.setState({ friendsList: filteredList });
+  }
+
+  renderFriendItems = () => {
+    const { friendsList } = this.state;
+    return (
+      <Row className="item-list">
+        {friendsList.map((item) => (
+          <FriendItem
+            key={item.id}
+            username={item.name}
+            id={item.id}
+            handleUnfollow={(id) => this.handleUnfollow(id)}
+          />
+        ))}
+      </Row>
+    );
+  }
+
+  render() {
+    const { friendsList } = this.state;
+    return (
+      <Container fluid className="page-wrapper">
+        <Row>
+          <Col md={12}>
+            <NavigationBar selected="Friends" />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={2} />
+          <Col md={8}>
+            <div className="content-wrapper">
+              <div className="title">
+                <PeopleAltOutlinedIcon className="icon" />
+                <p>Friends</p>
+                <p>
+                  (
+                  {friendsList.length}
+                  )
+                </p>
+              </div>
+              {this.renderFriendItems()}
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+export default FriendsPage;
