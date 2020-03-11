cf13a8b44ab3d3785a73e5de5fa8617553eed99e
Yuxuan Zhao <me@yuxuanz.com>
2020-03-10 20:56:05 -0600

    check travis again

--- backend/tests/test_comment_api.py
+++ backend/tests/test_comment_api.py
@@ -34,20 +34,23 @@ class TestCommentAPI:
         response = client.get("/posts/{}/comments".format(test_post.pk))
         assert response.status_code == 200
         assert response.data["query"] == "comments"
-        assert response.data["count"] is not None 
+        assert response.data["count"] is not None
         assert response.data["comments"] is not None
-        print(response.data)
         assert len(response.data["comments"]) > 0
+
         comment = response.data["comments"][0]
         assert comment["author"]["id"] == comment_user.get_full_user_id()
         assert comment["comment"] == test_comment.content
         client.logout()
 
+    def test_create_comment(self, client, test_user, test_host):
+        comment_user = User.objects.create_user(
+            username="commenter", password="ualberta01!", host=test_host)
+        no_access_user = User.objects.create_user(
+            username="noaccess", password="ualberta01!", host=test_host)
 
-
-
-
+        test_post = Post.objects.create(
+            author=test_user, title="post title", content="post content", visibility=PRIVATE, visibleTo=[comment_user.get_full_user_id()])
         
-
-    def test_create_comment(self, client, test_user):
         pass
+        # client.post("/posts/{}/comments".format(test_post.pk))
