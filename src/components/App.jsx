15771ba5d3f6ba5d64abdad39e8e68f34a0e5d28
Yuxuan Zhao <me@yuxuanz.com>
2020-03-12 17:47:50 -0600

    clean up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import Login from "./Login";
-import Homepage from "./Homepage"
 
 
 function App() {
-  return <Homepage/>;
+  return <Login/>;
 }
 
 export default App;
