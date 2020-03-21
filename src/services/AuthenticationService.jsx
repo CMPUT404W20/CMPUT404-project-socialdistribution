9fd39afd73654291111782ba41cfa5c4562a61f5
Yuxuan Zhao <yzhao9@ualberta.ca>
2020-03-20 20:36:16 -0600

    add logout (#135)
    Co-authored-by: Maharsh Patel <maharshmellow@users.noreply.github.com>

--- src/services/AuthenticationService.jsx
+++ src/services/AuthenticationService.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable object-shorthand */
 /* eslint-disable arrow-body-style */
 import axios from "axios";
+import Cookies from "js-cookie";
 
 export const loginUser = (username, password) => {
   return axios.post("/auth/login/", {
@@ -18,4 +20,13 @@ export const registerUser = (username, password) => {
 
 export const getCurrentUser = () => {
   return axios.get("/author/current/");
+};
+
+export const logoutUser = () => {
+  const csrf = Cookies.get("csrftoken");
+  const headers = {
+    "X-CSRFToken": csrf,
+  };
+
+  return axios.post("/auth/logout/", null, { headers: headers });
 };
