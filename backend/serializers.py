2ce8ab0f0a33d647c93dc1b346c4a7125c24d1ee
Yuxuan Zhao <me@yuxuanz.com>
2020-03-26 13:18:59 -0600

    add origin

--- backend/serializers.py
+++ backend/serializers.py
@@ -55,6 +55,7 @@ class PostSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(source="postId", read_only=True)
     unlisted = serializers.BooleanField(source="is_unlisted", read_only=True)
     source = serializers.URLField(source="get_source", read_only=True)
+    origin = serializers.URLField(source="get_source", read_only=True)
 
     def to_representation(self, instance):
         response = super().to_representation(instance)
