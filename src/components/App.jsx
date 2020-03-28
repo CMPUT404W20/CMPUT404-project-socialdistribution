7f32d2b29c80f4b9c87ffb85ab187478f28f27b6
ceciliaccwei <ceciliawei@yahoo.com>
2020-03-27 20:54:33 -0600

    eslint

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {
-  BrowserRouter, Route, Switch,
+  BrowserRouter, Route,
 } from "react-router-dom";
 import PropTypes from "prop-types";
 import Homepage from "./Homepage";
