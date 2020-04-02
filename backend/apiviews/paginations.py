9bafe7fd4af2299944fa0dcadf456a0c5cca2227
Yuxuan Zhao <me@yuxuanz.com>
2020-04-01 21:10:02 -0600

    add support for custom page size query

--- backend/apiviews/paginations.py
+++ backend/apiviews/paginations.py
@@ -5,26 +5,26 @@ from collections import OrderedDict
 
 
 class PostPagination(PageNumberPagination):
-    page_size = 50
+    page_size_query_param = "size"
 
     def get_paginated_response(self, data):
         return Response(OrderedDict([
             ('query', 'posts'),
             ('count', self.page.paginator.count),
-            ('size', self.page_size),
+            ('size', self.get_page_size(self.request)),
             ('next', self.get_next_link()),
             ('previous', self.get_previous_link()),
             ('posts', data)
         ]))
 
 class CommentPagination(PageNumberPagination):
-    page_size = 50
+    page_size_query_param = "size"
 
     def get_paginated_response(self, data):
         return Response(OrderedDict([
             ('query', 'comments'),
             ('count', self.page.paginator.count),
-            ('size', self.page_size),
+            ('size', self.get_page_size(self.request)),
             ('next', self.get_next_link()),
             ('previous', self.get_previous_link()),
             ('comments', data)
