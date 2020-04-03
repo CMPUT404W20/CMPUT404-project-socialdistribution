a78f054c6857189381ead52af7361fadf3fbe36b
Maharsh Patel <maharshmellow@users.noreply.github.com>
2020-04-02 21:40:48 -0600

    Click username to view profile page (#225)
    * add shadow for profile header
    
    * click username to view profile page
    
    * update test

--- src/components/post/PostView.jsx
+++ src/components/post/PostView.jsx
@@ -67,7 +67,8 @@ class PostView extends Component {
         const post = response.posts[i];
 
         newPost.username = post.author.displayName;
-        newPost.authorId = post.author.id;
+        newPost.authorId = post.author.id || "";
+        newPost.authorHost = post.author.host || "";
         newPost.title = post.title;
         newPost.content = post.content;
         newPost.published = post.published;
